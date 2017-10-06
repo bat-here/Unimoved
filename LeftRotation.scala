d5eb9cd95e5fbf829c856bf23d7a9780cb570016
shree-shubham <jaykr674@gmail.com>
2017-10-06 07:51:54 +0530

    Scala Support

---
+++ LeftRotation.scala
@@ -0,0 +1,38 @@
+package datastructure.arrays
+
+import scala.annotation.tailrec
+
+/**
+  * Created by before30 on 2016. 7. 26..
+  */
+object LeftRotation {
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner (System.in)
+    var n = sc.nextInt()
+    var times = sc.nextInt()
+    var arr = new Array[Int](n);
+
+    for(arr_i <- 0 to n-1) {
+      arr(arr_i) = sc.nextInt();
+    }
+
+    val integers = arr.toList
+    println(leftRotateNtimes(integers, times).mkString(" "))
+  }
+
+  def leftRotate(integers: List[Int]): List[Int] = {
+    (integers.head :: integers.tail.reverse).reverse
+  }
+
+//  @tailrec
+  def leftRotateNtimes(integers: List[Int], n: Int): List[Int] = {
+    val i = n % integers.length
+    val (left:List[Int], right:List[Int]) = integers.splitAt(i)
+
+    (left.reverse ::: right.reverse).reverse
+
+//    if (n == 0) integers
+//    else leftRotateNtimes(leftRotate(integers), n  - 1)
+  }
+
+}
